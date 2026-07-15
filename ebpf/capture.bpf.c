7fbfbb2d092abdb4c4191716ff9bea7c71b4e500
RocketDev <ma2014119@outlook.com>
2026-07-15 16:33:46 +0800

    fix(capture): use big macro to simplify code

--- ebpf/capture.bpf.c
+++ ebpf/capture.bpf.c
@@ -3,6 +3,7 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
+#include <linux/bpf_common.h>
 #include "ebpf_share.h"
 #include "utils/ebpf_logger.h"
 #include "utils/error.h"
@@ -38,33 +39,30 @@ BPF_PROG (seccomp_check_filter_entry, struct sock_filter *filter,
   uint32_t pid = bpf_get_current_pid_tgid ();
   uint32_t zero = 0;
   int err = 0;
-  scmp_arg *tmp = bpf_map_lookup_elem (&tmp_buf, &zero);
-  if (tmp == NULL)
-    ebpf_log_return (BPF_MAP_LOOKUP_FAILED, pid);
+  scmp_arg *tmp;
+  EBPF_EXPECT (NULL, NULL, NULL, (tmp = bpf_map_lookup_elem (&tmp_buf, &zero)),
+               pid);
 
   // This shouldn't happen. Just ignore this anyway
-  if (flen > BPF_MAXINSNS)
-    ebpf_log_return (FILTER_LEN_EXCEED, pid);
+  EBPF_EXPECT (NULL, NULL, NULL, flen <= BPF_MAXINSNS, pid);
 
-  err = bpf_map_update_elem (&unverified_filters, &pid, tmp, BPF_ANY);
-  if (err < 0)
-    ebpf_log_return (BPF_MAP_UPDATE_FAILED, pid);
+  EBPF_EXPECT (
+      NULL, NULL, NULL,
+      (err = bpf_map_update_elem (&unverified_filters, &pid, tmp, BPF_ANY))
+          >= 0,
+      pid);
 
-  scmp_arg *unverified = bpf_map_lookup_elem (&unverified_filters, &pid);
-  if (unverified == NULL)
-    {
-      bpf_map_delete_elem (&unverified_filters, &pid);
-      ebpf_log_return (BPF_MAP_LOOKUP_FAILED, pid);
-    }
+  scmp_arg *unverified;
+  EBPF_EXPECT (NULL, &unverified_filters, &pid,
+               (bpf_map_delete_elem (&unverified_filters, &pid)), pid);
 
   unverified->len = flen;
-  err = bpf_core_read (unverified->filters,
-                       unverified->len * sizeof (struct sock_filter), filter);
-  if (err < 0)
-    {
-      bpf_map_delete_elem (&unverified_filters, &pid);
-      ebpf_log_return (BPF_CORE_READ_FAILED, pid);
-    }
+  ;
+  EBPF_EXPECT (NULL, &unverified_filters, &pid,
+               err = bpf_core_read (
+                   unverified->filters,
+                   unverified->len * sizeof (struct sock_filter), filter),
+               pid);
   return 0;
 }
 
@@ -109,20 +107,14 @@ filter_mode (long ret, pid_t pid, scmp_event *event)
   event->ebpf_arch = EBPF_ARCH_OTHERS;
 #endif
 
-  scmp_arg *arg = bpf_map_lookup_elem (&unverified_filters, &pid);
-  if (arg == NULL)
-    {
-      bpf_ringbuf_discard (event, 0);
-      ebpf_log_return (BPF_MAP_LOOKUP_FAILED, pid);
-    }
+  scmp_arg *arg;
+  EBPF_EXPECT (event, 0, 0,
+               (arg = bpf_map_lookup_elem (&unverified_filters, &pid)), pid);
 
-  int err = bpf_core_read (&event->arg, sizeof (scmp_arg), arg);
-  if (err < 0)
-    {
-      bpf_ringbuf_discard (event, 0);
-      bpf_map_delete_elem (&unverified_filters, &pid);
-      ebpf_log_return (BPF_CORE_READ_FAILED, pid);
-    }
+  int err;
+  EBPF_EXPECT (
+      event, &unverified_filters, &pid,
+      (err = bpf_core_read (&event->arg, sizeof (scmp_arg), arg) >= 0), pid);
 
   bpf_ringbuf_submit (event, 0);
   bpf_map_delete_elem (&unverified_filters, &pid);
@@ -138,10 +130,11 @@ BPF_PROG (seccomp_ret, uint32_t op, uint32_t flags, void *uargs, long ret)
   if (op == SECCOMP_GET_ACTION_AVAIL || op == SECCOMP_GET_NOTIF_SIZES)
     return 0;
 
-  scmp_event *event
-      = bpf_ringbuf_reserve (&scmp_events, sizeof (scmp_event), 0);
-  if (event == NULL)
-    ebpf_log_return (BPF_RINGBUF_RESERVE_FAILED, pid);
+  scmp_event *event;
+  EBPF_EXPECT (
+      NULL, NULL, NULL,
+      (event = bpf_ringbuf_reserve (&scmp_events, sizeof (scmp_event), 0)),
+      pid);
   event->pid = pid;
 
   if (op == SECCOMP_SET_MODE_STRICT)
--- include/utils/ebpf_logger.h
+++ include/utils/ebpf_logger.h
@@ -1,10 +1,15 @@
 #ifndef EBPF_LOGGER_H
 #define EBPF_LOGGER_H
 
-#define ebpf_log_return(fmt, ...)                                             \
-  {                                                                           \
-    bpf_printk (fmt, __VA_ARGS__);                                            \
-    return 0;                                                                 \
-  }
+#define EBPF_EXPECT(event, map, obj, cond, ...)                               \
+  if (!(cond))                                                                \
+    {                                                                         \
+      _Generic ((event),                                                      \
+          scmp_event *: bpf_ringbuf_discard (event, 0),                       \
+          default: 0);                                                        \
+      _Generic ((obj), pid_t *: bpf_map_delete_elem (map, obj), default: 0);  \
+      bpf_printk ("Unexpected !" #cond " in process %d", __VA_ARGS__);        \
+      return 0;                                                               \
+    }
 
 #endif
