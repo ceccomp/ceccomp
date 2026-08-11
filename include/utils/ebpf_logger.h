0c055c2e1329fd6c05e4d053a807252facc8eb8d
dbgbgtf <dudududuMaxVer@outlook.com>
2026-08-11 12:06:44 +0800

    feat(capture): add file and line info in ebpf_log

--- include/utils/ebpf_logger.h
+++ include/utils/ebpf_logger.h
@@ -1,21 +1,25 @@
 #ifndef EBPF_LOGGER_H
 #define EBPF_LOGGER_H
 
+#define EBPF_STRINGIFY_IMPL(x) #x
+#define EBPF_STRINGIFY(x) EBPF_STRINGIFY_IMPL (x)
+#define EBPF_AT __FILE__ ":" EBPF_STRINGIFY (__LINE__) ": "
+
 #define EBPF_IF(cond)                                                         \
   if ((tmp_cond = (cond)))                                                    \
-    bpf_printk ("Unexpected " #cond);                                         \
+    bpf_printk (EBPF_AT "Unexpected " #cond);                                 \
   if (tmp_cond)
 
 #define EBPF_IF_PID(cond, pid)                                                \
   if ((tmp_cond = (cond)))                                                    \
-    bpf_printk ("Unexpected " #cond " in process %d", pid);                   \
+    bpf_printk (EBPF_AT "Unexpected " #cond " in process %d", pid);           \
   if (tmp_cond)
 
 #define EBPF_LOG_IF_PID(cond, pid)                                            \
   do                                                                          \
     {                                                                         \
       if (cond)                                                               \
-        bpf_printk ("Unexpected " #cond " in process %d", pid);               \
+        bpf_printk (EBPF_AT "Unexpected " #cond " in process %d", pid);       \
     }                                                                         \
   while (0)
 
