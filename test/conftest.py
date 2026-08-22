afcb839ea47c4b26417d6c1109ce3566c3e43997
RocketDev <ma2014119@outlook.com>
2026-08-22 22:48:03 +0800

    feat(test): display xfail detail

--- test/conftest.py
+++ test/conftest.py
@@ -5,13 +5,18 @@ import pytest
 def errns() -> SimpleNamespace:
     return SimpleNamespace()
 
+def fail_or_xfail(report) -> bool:
+    return (
+        report.skipped and getattr(report, "wasxfail", None) is not None
+    ) or report.failed
+
 # hook pytest report to print stderr
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     outcome = yield
     report = outcome.get_result()
 
-    if report.when == 'call' and report.failed:
+    if report.when == 'call' and fail_or_xfail(report):
         ns = item.funcargs.get('errns')
         if hasattr(ns, 'stderr'):
             report.sections.append(('Process Standard Error', ns.stderr))
